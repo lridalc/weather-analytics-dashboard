a86d4239338dd4c74d3d48bf0961dff223097893
lridalc <lridalcdev@gmail.com>
2026-04-12 16:37:25 +0200

    test: add unit test placeholder

---
+++ tests/unit/test_placeholder.py
@@ -0,0 +1,6 @@
+"""Placeholder test to keep CI green until real unit tests are added."""
+
+
+def test_placeholder():
+    """This test will be replaced with real unit tests in future phases."""
+    assert True
