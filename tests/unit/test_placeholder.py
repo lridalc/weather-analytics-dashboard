55a3f58fc47443f8db6e50e10096c01af92aad51
lridalc <lridalcdev@gmail.com>
2026-04-14 18:03:07 +0200

    test(config): add settings loading tests

--- tests/unit/test_placeholder.py
+++
@@ -1,6 +0,0 @@
-"""Placeholder test to keep CI green until real unit tests are added."""
-
-
-def test_placeholder():
-    """This test will be replaced with real unit tests in future phases."""
-    assert True
