b3f6abbd258b5e4c42c709270a0a1d7f1ca2e655
lridalc <lridalcdev@gmail.com>
2026-04-17 16:36:46 +0200

    feat(cli): configure return code 0 when invoked with no args

--- src/weather_analytics_dashboard/presentation/cli/main.py
+++ src/weather_analytics_dashboard/presentation/cli/main.py
@@ -1,6 +1,10 @@
 import click
 
 
-@click.group()
-def cli():
-    pass
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    """Weather Analytics Dashboard CLI."""
+    if ctx.invoked_subcommand is None:
+        click.echo(ctx.get_help())
+        # Implicit ctx.exit(0)
